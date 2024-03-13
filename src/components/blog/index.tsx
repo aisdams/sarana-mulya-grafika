fc63a7fd908c4925689349f323b30bb11e1f9aac
xlwzzzy <marioaistwice@gmail.com>
2024-03-13 15:16:11 +0700

    add blog details

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,155 +1,157 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 
 interface Blog {
-    id: string;
-    title: string;
-    thumbnailSrc: string;
-    createdAt: string;
-    slug: string;
-  }
+  id: string;
+  title: string;
+  thumbnailSrc: string;
+  createdAt: string;
+  slug: string;
+}
 
-  export const getServerSideProps: GetServerSideProps = async ({
-    locale,
-    query,
-  }) => {
-    const res = await fetch('https://api.cms.neelo.id/api/posts');
-    const data = await res.json();
-    const page = query.page ? parseInt(query.page as string) : 1;
-    const perPage = 2;
-    const offset = (page - 1) * perPage;
-  
-    const blogs: Blog[] = data.data.map((blog: any) => ({
-      id: blog.id,
-      title: blog.title,
-      thumbnailSrc: `https://api.cms.neelo.id/${blog.thumbnailSrc}`,
-      slug: blog.slug,
-      createdAt: blog.createdAt,
-    }));
-  
-    const totalCount = blogs.length;
-  
-    return {
-      props: {
-        blogs: blogs.slice(offset, offset + perPage),
-        totalCount,
-        currentPage: page,
-      },
-    };
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  query,
+}) => {
+  const res = await fetch('https://api.cms.neelo.id/api/posts');
+  const data = await res.json();
+  const page = query.page ? parseInt(query.page as string) : 1;
+  const perPage = 2;
+  const offset = (page - 1) * perPage;
+
+  const blogs: Blog[] = data.data.map((blog: any) => ({
+    id: blog.id,
+    title: blog.title,
+    thumbnailSrc: `https://api.cms.neelo.id/${blog.thumbnailSrc}`,
+    slug: blog.slug,
+    createdAt: blog.createdAt,
+  }));
+
+  const totalCount = blogs.length;
+
+  return {
+    props: {
+      blogs: blogs.slice(offset, offset + perPage),
+      totalCount,
+      currentPage: page,
+    },
   };
+};
 
 export default function Blog({
-    blogs,
-    totalCount,
-    currentPage,
-  }: {
-    blogs: Blog[];
-    totalCount: number;
-    currentPage: number;
-  }) {
-    const router = useRouter();
-    const totalPages = Math.ceil(totalCount / 2);
-    const [isActive, setIsActive] = useState(true);
-  
-    const navigateToBlogDetail = (slug: string) => {
-      router.push(`/blog/blog-details/${slug}`);
+  blogs,
+  totalCount,
+  currentPage,
+}: {
+  blogs: Blog[];
+  totalCount: number;
+  currentPage: number;
+}) {
+  const router = useRouter();
+  const totalPages = Math.ceil(totalCount / 2);
+  const [isActive, setIsActive] = useState(true);
+
+  const navigateToBlogDetail = (slug: string) => {
+    router.push(`/blog/blog-details/${slug}`);
+  };
+  const formatDate = (dateString: any) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit',
     };
-    const formatDate = (dateString: any) => {
-      const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'long',
-        day: '2-digit',
-      };
-      const formattedDate = new Date(dateString).toLocaleDateString(
-        'en-US',
-        options
-      );
-      return formattedDate;
-    };
-  
-    const renderPagination = () => {
-      const pageNumbers = [];
-      for (let i = 1; i <= totalPages; i++) {
-        pageNumbers.push(i);
-      }
-  
-      return (
-        <nav aria-label="Page navigation example">
-          <ul className="inline-flex -space-x-px">
-            <li>
-              <a
-                href="#"
-                className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Previous
-              </a>
-            </li>
-            {pageNumbers.map((number) => (
-              <li key={number}>
-                <Link
-                  href={`/blog/?page=${number}`}
-                  className={`${
-                    number === currentPage
-                      ? 'bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700'
-                      : 'bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700'
-                  } leading-tight py-2 px-3 dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white`}
-                >
-                  {number}
-                </Link>
-              </li>
-            ))}
-            <li>
-              <a
-                href="#"
-                className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Next
-              </a>
-            </li>
-          </ul>
-        </nav>
-      );
-    };
-  
+    const formattedDate = new Date(dateString).toLocaleDateString(
+      'en-US',
+      options
+    );
+    return formattedDate;
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+
     return (
-      <>
-        <div className="grid w-[min(90%,1280px)] mx-auto">
-            <h3>News & Blog</h3>
-            <h1>Latest News & <span className='text-blueseaprimary'>Blog</span></h1>
-  
-          <div className="my-20 grid lg:grid-cols-3 gap-5 mx-auto ">
-            {blogs.map((blog) => (
-              <div
-                key={blog.id}
-                className="w-[25rem] shadow-2xl pb-10 rounded-tl-xl rounded-tr-xl overflow-hidden"
+      <nav aria-label="Page navigation example">
+        <ul className="inline-flex -space-x-px">
+          <li>
+            <a
+              href="#"
+              className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Previous
+            </a>
+          </li>
+          {pageNumbers.map((number) => (
+            <li key={number}>
+              <Link
+                href={`/blog/?page=${number}`}
+                className={`${
+                  number === currentPage
+                    ? 'bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700'
+                    : 'bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700'
+                } leading-tight py-2 px-3 dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white`}
               >
-                <Image
-                  src={blog.thumbnailSrc}
-                  alt=""
-                  width={600}
-                  height={400}
-                  className="object-cover w-full h-56 mb-10"
-                />
-                <div className="grid pl-5">
-                  <h3 className="font-light">{formatDate(blog.createdAt)}</h3>
-                  <div
-                    className="font-bold text-base-blue mt-5 text-xl tracking-wider cursor-pointer"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      navigateToBlogDetail(blog.slug);
-                    }}
-                  >
-                    {blog.title}
-                  </div>
+                {number}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <a
+              href="#"
+              className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
+  return (
+    <>
+      <div className="grid w-[min(90%,1280px)] mx-auto">
+        <h3>News & Blog</h3>
+        <h1>
+          Latest News & <span className="text-blueseaprimary">Blog</span>
+        </h1>
+
+        <div className="my-20 grid lg:grid-cols-3 gap-5 mx-auto ">
+          {blogs.map((blog) => (
+            <div
+              key={blog.id}
+              className="w-[25rem] shadow-2xl pb-10 rounded-tl-xl rounded-tr-xl overflow-hidden"
+            >
+              <Image
+                src={blog.thumbnailSrc}
+                alt=""
+                width={600}
+                height={400}
+                className="object-cover w-full h-56 mb-10"
+              />
+              <div className="grid pl-5">
+                <h3 className="font-light">{formatDate(blog.createdAt)}</h3>
+                <div
+                  className="font-bold text-base-blue mt-5 text-xl tracking-wider cursor-pointer"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigateToBlogDetail(blog.slug);
+                  }}
+                >
+                  {blog.title}
                 </div>
               </div>
-            ))}
-          </div>
-          <div className="max-w-2xl mx-auto">{renderPagination()}</div>
+            </div>
+          ))}
         </div>
-      </>
-    );
-  }
+        <div className="max-w-2xl mx-auto">{renderPagination()}</div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/blog/blog-details/[slug].tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import BlogDetailsImage from '/public/img/blog-details.jpg';
+import { MdDateRange } from 'react-icons/md';
+import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
+import NextImage from 'next/image';
+import Image from 'next/image';
+
+interface BlogDetails {
+  thumbnailSrc: string;
+  createdAt: string;
+  title: string;
+  content: string;
+}
+
+export default function Index() {
+  const [blogDetails, setBlogDetails] = useState<BlogDetails | null>(null);
+  const router = useRouter();
+  const { slug } = router.query;
+
+  useEffect(() => {
+    const fetchBlogDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://api.cms.neelo.id/api/posts/${slug}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setBlogDetails(data.data);
+        } else {
+          throw new Error('Failed to fetch blog details');
+        }
+      } catch (error) {
+        console.error('Error fetching blog details:', error);
+      }
+    };
+
+    if (slug) {
+      fetchBlogDetails();
+    }
+  }, [slug]);
+
+  if (!blogDetails) {
+    return (
+      <div>
+        {' '}
+        <div className="relative">
+          <Image
+            src={BlogDetailsImage}
+            className="w-full h-[450px] mb-20 mx-auto grid text-center object-cover opacity-55"
+            alt=""
+          />
+          <div className="absolute top-1/2 left-0 right-0 text-white">
+            <h3 className="tracking-[6px] mb-3">Blog Details</h3>
+            <h1 className="font-bold lg:text-5xl text-3xl">SMG!</h1>
+          </div>
+        </div>
+        <div className="text-center font-bold">
+          Something Error OR Doesn{`'`}t Have Details BLOG......
+        </div>
+      </div>
+    );
+  }
+
+  const formatDate = (dateString: any) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit',
+    };
+    const formattedDate = new Date(dateString).toLocaleDateString(
+      'en-US',
+      options
+    );
+    return formattedDate;
+  };
+
+  return (
+    <>
+      <div className="relative">
+        <Image
+          src={BlogDetailsImage}
+          alt=""
+          className="w-full h-[450px] mb-20 mx-auto grid text-center object-cover opacity-55"
+        />
+        <div className="absolute top-1/2 right-0 left-0 mx-auto grid w-full  text-center text-white">
+          <h3 className="tracking-[6px] mb-3">Blog Details</h3>
+          <h1 className="font-bold lg:text-5xl text-3xl">SMG!</h1>
+        </div>
+      </div>
+
+      <article className="mx-auto justify-center items-center grid place-content-center lg:w-[min(55%,1280px)] w-[min(90%,1280px)] my-24 prose prose-lg">
+        <img
+          src={`https://api.cms.neelo.id/${blogDetails.thumbnailSrc}`}
+          alt=""
+          className="w-full h-auto"
+        />
+        <div className="flex gap-5 mt-5">
+          <div className="flex items-center gap-2">
+            <FaUser /> Admin
+          </div>
+
+          <div className="flex items-center gap-2">
+            <MdDateRange /> {formatDate(blogDetails.createdAt)}
+          </div>
+        </div>
+
+        <h1 className="font-medium mt-5 mb-0 text-3xl">{blogDetails.title}</h1>
+        <div
+          className="mt-2 tfont-light text-sm"
+          dangerouslySetInnerHTML={{ __html: blogDetails.content }}
+        ></div>
+      </article>
+    </>
+  );
+}
